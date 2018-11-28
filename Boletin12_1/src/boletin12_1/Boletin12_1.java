bf3d3521db8f73fb0214ab21f99322021a9232ce
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-11-28 11:41:57 +0100

    Boletin12

---
+++ Boletin12_1/src/boletin12_1/Boletin12_1.java
@@ -0,0 +1,17 @@
+package boletin12_1;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin12_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Garaje obx=new Garaje();
+        obx.programa();
+    }
+    
+}
